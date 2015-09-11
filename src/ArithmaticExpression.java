fb203ae8b75c16585846baa6e8b5fc0e55f6e73a
MohamedFawzy <mfawzy22@gmail.com>
2015-09-11 16:17:13 +0200

    add problem 1 ArithmaticExpression

---
+++ src/ArithmaticExpression.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+/**
+ * 
+ * @author Mohamed Fawzy
+ * @copyright GPL
+ */
+public class ArithmaticExpression {
+	
+	public static int evalRPN(String[] tokens){
+		int returnValue=0;
+		String operations = "+-*/"; // All Arithmatic operations
+		// we push Expression and values into statck then return them back based on priroity for it 
+		Stack<String> stack = new Stack<String>();
+		for(String t: tokens){
+			if(!operations.contains(t)){
+				stack.push(t);
+			}else{
+				int a = Integer.valueOf(stack.pop());
+				int b = Integer.valueOf(stack.pop());
+				switch(t){
+					case "+":
+							stack.push(String.valueOf(a+b));
+						break;
+					case "-":
+							stack.push(String.valueOf(a-b));
+						break;
+					case "*":
+							stack.push(String.valueOf(a*b));
+						break;
+					case "/":
+							stack.push(String.valueOf(a/b));
+						break;
+					
+				}
+			}
+		}
+		returnValue = Integer.valueOf(stack.pop());
+		return returnValue;
+	}
+}
---
+++ src/ArithmaticExpressionTest.java
@@ -0,0 +1,9 @@
+
+public class ArithmaticExpressionTest {
+	
+	public static void main(String[] args){
+		String[] tokens = new String[] {"2","1","+","3","*"};
+		ArithmaticExpression ae = new ArithmaticExpression();
+		System.out.println(ae.evalRPN(tokens));
+	}
+}
